1c0480f2c8c3d3f2f2b80fc2e9bad08d453c1942
Dominic Shaw <dominicshaw@hotmail.com>
2017-05-16 15:03:15 +0100

    added image column

---
+++ Skillz/ViewModel/SkillToImageConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using Skillz.Models;
+
+namespace Skillz.ViewModel
+{
+    public class SkillToImageConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var attachmentType = (SkillTypes) Enum.Parse(typeof(SkillTypes), value.ToString());
+
+            return string.Format("/Skillz;component/Images/{0}-icon.png", attachmentType);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
