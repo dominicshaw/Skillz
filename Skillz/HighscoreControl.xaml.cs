93375c3783625a01df28ff4fddecb2e617725db3
Dominic Shaw <dominicshaw@hotmail.com>
2017-05-16 14:48:05 +0100

    added usercontrol

---
+++ Skillz/HighscoreControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Skillz
+{
+    /// <summary>
+    /// Interaction logic for HighscoreControl.xaml
+    /// </summary>
+    public partial class HighscoreControl : UserControl
+    {
+        public HighscoreControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
